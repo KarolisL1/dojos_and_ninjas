31ac4ce9895f6dc401b824a51929a7c92156b74a
karolisliepa <Karolis.Liepa@gmail.com>
2022-02-17 21:34:41 -0500

    dojos and ninjas with FK

---
+++ flask_app/controllers/dojos.py
@@ -0,0 +1,23 @@
+from flask_app import app
+from flask import render_template,redirect,request,session
+from flask_app.models.dojo import Dojo
+
+@app.route("/")
+def index():
+    return redirect('/dojos')
+
+@app.route('/dojos')
+def all_dojos():
+    dojo = Dojo.get_all()
+    return render_template('index.html', dojo=dojo)
+
+@app.route('/create_dojo', methods=["POST"])
+def create_dojo():
+    Dojo.save(request.form)
+    return redirect('/dojos')
+
+@app.route('/dojos/<int:dojo_id>')
+def single_dojo(dojo_id):
+    data = {'dojo_id': dojo_id}
+    dojo = Dojo.get_one_dojo(data)
+    return render_template('dojo_show.html', dojo=dojo)
---
+++ flask_app/models/dojo.py
@@ -0,0 +1,51 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import ninja
+
+class Dojo():
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.ninjas = []
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM dojos;"
+        # make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL('dojos_and_ninjas_schema').query_db(query)
+        # Create an empty list to append our instances of friends
+        dojos = []
+        # Iterate over the db results and create instances of friends with cls.
+        for dojo in results:
+            dojos.append( cls(dojo) )
+        return dojos
+
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO dojos ( name , created_at, updated_at ) VALUES ( %(name)s , NOW() , NOW() );"
+        # data is a dictionary that will be passed into the save method from server.py
+
+        return connectToMySQL('dojos_and_ninjas_schema').query_db( query, data )
+
+    @classmethod
+    def get_one_dojo(cls, data):
+        query = "SELECT * FROM dojos LEFT JOIN ninjas ON ninjas.dojo_id = dojos.id WHERE dojos.id = %(dojo_id)s; "
+
+        results = connectToMySQL('dojos_and_ninjas_schema').query_db( query, data )
+
+        dojo = Dojo(results[0])
+        for result in results:
+            print(result)
+            ninja_data = {
+                'id': result['ninjas.id'],
+                'first_name': result['first_name'],
+                'last_name': result['last_name'],
+                'age': result['age'],
+                'created_at': result['ninjas.created_at'],
+                'updated_at': result['ninjas.updated_at'],
+                'dojo_id': result['dojo_id']
+            }
+            dojo.ninjas.append(ninja.Ninja(ninja_data))
+
+        return dojo
